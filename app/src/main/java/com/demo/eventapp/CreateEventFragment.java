5d34220b9c569f7b8af2690b548a3017ba410e3d
jia2015 <richard-ljx@hotmail.com>
2017-03-11 11:27:37 -0500

    first commit

---
+++ app/src/main/java/com/demo/eventapp/CreateEventFragment.java
@@ -0,0 +1,37 @@
+package com.demo.eventapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreateEventFragment extends Fragment {
+
+    public CreateEventFragment() {
+        // Required empty public constructor
+    }
+
+    public static CreateEventFragment newInstance() {
+        CreateEventFragment fragment = new CreateEventFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_event, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/demo/eventapp/LocationFragment.java
@@ -0,0 +1,38 @@
+package com.demo.eventapp;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LocationFragment extends Fragment {
+
+
+    public LocationFragment() {
+        // Required empty public constructor
+    }
+
+    public static LocationFragment newInstance() {
+        LocationFragment fragment = new LocationFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_location, container, false);
+    }
+
+}
